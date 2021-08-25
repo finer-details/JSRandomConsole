b0617a5d89dffc095b02acbf4a9b24f42f0e30c2
David Devlin <david.mcp77@gmail.com>
2021-08-25 14:59:40 +0100

    Added javascript file, small edit to html file

---
+++ RandomMessages.js
@@ -0,0 +1,11 @@
+//Random Message Generator Project.
+
+const tomorrowsWeatherArray = ["Sunny", "Cloudy", "Rainy", "Snowing", "Windy"];
+
+const howToDressArray = ["T-shirt & Shorts", "Jumper", "Rain Coat", "Gloves & Scarf", "Stay inside"];
+
+const howFarToWalkDogArray = ["10 minutes", "20 minutes", "30 minutes", "50 minutes", "A full Hour!"];
+
+
+const fullMessageArray = [];
+
